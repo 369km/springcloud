debad2b5d04cc1daa5f3888792d4ecbc29def8e3
yfd <evof@qq.com>
2019-08-21 11:40:55 +0800

    fix

--- parent/service-comsume/src/main/java/com/foo/springcloud/service/comsume/service/HelloServiceImpl.java
+++ parent/service-comsume/src/main/java/com/foo/springcloud/service/comsume/service/HelloServiceImpl.java
@@ -1,11 +1,15 @@
 package com.foo.springcloud.service.comsume.service;
 
 import com.foo.springcloud.service.product.feign.FooHelloFeign;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloServiceImpl implements HelloService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloServiceImpl.class);
+
     @Autowired
     private FooHelloFeign fooHelloFeign;
 
@@ -16,7 +20,7 @@ public class HelloServiceImpl implements HelloService {
 
     private String build(String arg1, String args2) {
         String result = arg1 + args2;
-        System.out.println(result);
+        LOGGER.info("result:{}", result);
         return result;
     }
 }
--- parent/service-product/web/src/main/java/com/foo/springcloud/service/product/web/controller/HelloController.java
+++ parent/service-product/web/src/main/java/com/foo/springcloud/service/product/web/controller/HelloController.java
@@ -1,25 +1,28 @@
 package com.foo.springcloud.service.product.web.controller;
 
 import com.foo.springcloud.service.product.api.HelloApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 public class HelloController implements HelloApi {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
     @Value("${server.port}")
     private String port;
+
     @Override
     public String hello() {
-        //模拟业务处理耗时,可能会出现Hystrix Readed time out
-        long time=(long) (Math.random()*3000);
+        //do buz time
+        long time = (long) (Math.random() * 3000);
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(time);
-        return "foo : "+port;
+        String result = "foo : " + port;
+        LOGGER.info("result:{},use-time:{}", result, time);
+        return result;
     }
 }
