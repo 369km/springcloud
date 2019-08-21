8e2fcfa462fb4ccb9f406fb36026380fa8609e8e
fudong.yang <fudong.yang@socialcredits.cn>
2019-08-21 08:27:30 +0800

    fix

--- parent/service-product/web/src/main/java/com/foo/springcloud/service/product/web/controller/HelloController.java
+++ parent/service-product/web/src/main/java/com/foo/springcloud/service/product/web/controller/HelloController.java
@@ -4,12 +4,21 @@ import com.foo.springcloud.service.product.api.HelloApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController implements HelloApi {
     @Value("${server.port}")
     private String port;
     @Override
     public String hello() {
+        long time=(long) (Math.random()*3000);
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(time);
         return "foo : "+port;
     }
 }
